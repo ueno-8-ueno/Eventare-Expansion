13bfb3dac2141b303c55e85d501059a33074a303
uuuu-8 <104149556+ueno-8-ueno@users.noreply.github.com>
2024-01-03 22:26:57 +0900

    [Add]Genreモデルの作成

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,6 @@
 class Event < ApplicationRecord
-    belongs_to :members
+    belongs_to :member
+    belongs_to :genre
 
     validates :name, presence: true
     validates :introduction, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_03_131001) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_03_132338) do
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -59,6 +59,12 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_03_131001) do
     t.string "start_at", null: false
     t.string "end_at", null: false
     t.boolean "is_done", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "genres", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
