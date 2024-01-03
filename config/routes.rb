ec6f10dd94238c7169e7910fe9c803b0678c3044
uuuu-8 <104149556+ueno-8-ueno@users.noreply.github.com>
2024-01-03 17:59:55 +0900

    [Finish]認証機能

--- config/routes.rb
+++ config/routes.rb
@@ -10,21 +10,21 @@ Rails.application.routes.draw do
     sessions: 'public/sessions'
   }
 
-  # 管理者認証ルーティング
-  devise_for :admin, skip: [:registrations, :passwords], controllers: {
-    sessions: "admin/sessions"
-  }
-
   # root_path
   root to: 'public/homes#top'
-  
 
   # 会員ルーティング
   scope module: :public do
     get '/about' => 'homes#about', as: 'about'
   end
 
+
+  # 管理者認証ルーティング
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
   # 管理者ルーティング
   namespace :admin do
+    get '/' => 'homes#top'
   end
 end
